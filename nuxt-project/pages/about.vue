40b416613498114a1e00da7aa1dc74ca176153bb
Jonathan Doucette <jon@digitalforgedesign.com>
2022-12-06 00:03:08 -0500

    first commit

---
+++ nuxt-project/pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat sequi
+      dolore id magni, eveniet quisquam, temporibus iusto eius facilis officia
+      similique officiis iure tenetur distinctio ullam? Quisquam molestias
+      dolorem iste!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, sit
+      aspernatur suscipit non quibusdam dolorem nostrum est error necessitatibus
+      eligendi? A vitae maiores autem id odit nesciunt voluptatem impedit quo.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ nuxt-project/pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h2>Products</h2></div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
